a9c9c64605ee16f2838527c9a29ef712e2bf2696
Krys McRae <kmmcrae1@aggies.ncat.edu>
2018-09-05 15:33:45 -0400

    ExtraCredit

--- Connect 4/src/connect/pkg4/Connect4.java
+++ Connect 4/src/connect/pkg4/Connect4.java
@@ -5,20 +5,23 @@
  */
 package connect.pkg4;
 
+import java.util.Random;
+
 /**
  *
  * @author krystophermcrae
  */
 public class Connect4 {
-       private static char[][] gameBoard=new char[6][7];
+       public static char[][] gameBoard=new char[6][7];
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        gameBoard= createBoard('k');
-        printBoard(gameBoard);
+        gameBoard= createBoard('b');
+        printBoard();
+        playGame(gameBoard);
         
     }
     public static char[][] createBoard(char input){
@@ -29,12 +32,53 @@ public class Connect4 {
         }
         return gameBoard;
     }
-    public static void printBoard(char[][]gameBoard){
-    for(int i=0;i<6;i++){
+    public static void printBoard(){
+        for(int i=0;i<6;i++){
             for(int j=0;j<7;j++){
                 System.out.print(gameBoard[i][j]+"  ");
             }
             System.out.println("");
         }
-}
+    }
+    public static void takeTurn(char currentPlayer){
+        Random rand=new Random();
+        int c =rand.nextInt(6);
+        int r=5;
+        boolean turnCheck=false;
+        while(turnCheck==false){
+        if (gameBoard[r][c]=='b'){
+            gameBoard[r][c]=currentPlayer;
+            turnCheck=true;
+        }else{
+            r--;
+        }
+        if(r==-1){
+            c =rand.nextInt(6);
+            r=5;
+        }
+    }
+    }
+    public static char playGame(char gameBoard[][]){
+        boolean red=true;
+        boolean yellow =false;
+        int i =0;
+        while(i<11){
+            if(red){
+                takeTurn('r');
+                printBoard();
+                System.out.println("");
+                red=false;
+                yellow=true;
+            }else if(yellow){
+                takeTurn('y');
+                printBoard();
+                System.out.println("");
+                red=true;
+                yellow=false;
+            }
+        i++;
+                }
+        
+        return 't';
+    }
 }
